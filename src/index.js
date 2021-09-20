894fb81c7d9878379b7b20aa0c50366abe5a41b5
coderlore <coderlore@gmail.com>
2021-09-19 22:49:40 -1000

    Initial build

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+
+class ToDo {
+    constructor(title, description, dueDate, priority, notes) {
+        this.title = title; 
+        this.description = description;
+        this.dueDate = dueDate;
+        this.notes = notes;
+    };
+};
+
+//function add to todo list
+
+//function for setting tasks as complete
+
+//function changing todo prioritys
