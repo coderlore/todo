9732fde0e564adf86eaeb3122f1d5f49ba06337f
coderlore <coderlore@gmail.com>
2021-10-12 08:28:30 -1000

    Created add and display function

--- src/addToDo.js
+++ src/addToDo.js
@@ -1,20 +1,47 @@
-const todoList = [];
-const list = document.querySelector('#tasks');
+
 
 function addToDo() {
-
+    const todoList = [];
+    const list = document.querySelector('#tasks');
     class ToDo {
         constructor(task) {
-            this.task = task; 
+            this.ulTask = task; 
         };
 
         add() {
-            const taskInput = document.querySelector('#inputText').nodeValue;
+            const taskInput = document.querySelector('#inputText').value;
+            if (taskInput == "") {
+                alert('No task was entered. Try again!');
+            } else {
+                const taskObject = {
+                    id: todoList.length,
+                    taskInput: taskInput,
+                    isDone: false,
+                }
+            todoList.unshift(taskObject);
+            this.display();
+            document.querySelector('#inputText').value = '';
+            }
         }
+
+        /*display() {
+            this.ulTask.innerHTML = '';
+            
+            todoList.forEach((object_item) => {
+                const liTask = document.createElement('li');
+                const delBtn = document.createElement('i');
+
+                liTask.innerText = object_item.taskInput;
+                liTask.setAttribute('data-id', object_item.id);
+                delBtn.setAttribute('data-id', object_item.id);
+                delBtn.classList.add('far', 'far-trash-alt');
+                //liTask.appendChild(delBtn);
+            }
+        }*/
     };
 
     myToDoList = new ToDo(list);
-    document.querySelector('.addBtn').addEventListener('click', function() {
+    document.querySelector('#addBtn').addEventListener('click', function() {
     myToDoList.add()
     })
 }
--- src/home.js
+++ src/home.js
@@ -28,6 +28,68 @@ function loadHome() {
     // Now add in the JS part
     
     //addToDo()
+    const todoList = [];
+
+    class ToDo {
+        constructor(task) {
+            this.ulTask = task; 
+        };
+
+        add() {
+            const taskInput = document.querySelector('#inputText').value;
+            if (taskInput == "") {
+                alert('No task was entered. Try again!');
+            } else {
+                const taskObject = {
+                    id: todoList.length,
+                    taskInput: taskInput,
+                    isDone: false,
+                }
+            todoList.unshift(taskObject);
+            this.display();
+            document.querySelector('#inputText').value = '';
+            }
+        }
+
+        display() {
+            this.ulTask.innerHTML = '';
+            
+            todoList.forEach((object_item) => {
+                const liTask = document.createElement('li');
+                const delBtn = document.createElement('i');
+
+                liTask.innerText = object_item.taskInput;
+                liTask.setAttribute('data-id', object_item.id);
+                delBtn.setAttribute('data-id', object_item.id);
+                delBtn.classList.add('far', 'far-trash-alt');
+                liTask.appendChild(delBtn);
+
+                delBtn.addEventListener('click', function(e) {
+                    const deleteId = e.target.getAttribute('data-id');
+                    myToDoList.deleteElement(deleteId);
+                })
+
+                liTask.addEventListener('click', function(e) {
+                    const selectId = e.target.getAttribute('data-id');
+                    myToDoList.done_undone(selectId);
+                })
+
+                if (object_item.isDone) {
+                    liTask.classList.add('checked');
+                }
+
+                this.ulTask.appendChild(liTask);
+            })
+        }
+    };
+
+    const list = document.querySelector('#tasks');
+    let myToDoList = [];
+    myToDoList = new ToDo(list);
+    document.querySelector('#addBtn').addEventListener('click', function() {
+    myToDoList.add()
+    })
+
 }
 
 export default loadHome
